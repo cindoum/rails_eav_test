74208e2836ab39ab6ec2926c8974fc4a782541ae
Dominic Lapointe <dom.dlapointe@gmail.com>
2014-11-26 16:32:08 +0000

    keep removing raw sql and create ruby class

--- lib/propify/acts_as_propify.rb
+++ lib/propify/acts_as_propify.rb
@@ -10,8 +10,8 @@ module Propify
     
     module ClassMethods
         def act_as_propify(options = {})
-            create_propify_tables self.table_name
-            create_propify_classes self.table_name
+            create_propify_tables
+            create_propify_classes
             
             include Propify::ActsAsPropify::LocalInstanceMethods
         end
@@ -24,41 +24,44 @@ module Propify
             propify_create_inner name
         end
         
-        def propify_delete id, name
-            raise "Cannot remove propify without an id or a name" unless !id.nil? || name.blank?
-            propify_delete_inner seld.table_name, id, name
+        def propify_delete name
+            propify_delete_inner name
         end
         
         protected
-            def propify_delete_inner table_name, id, name
-                
+            def propify_delete_inner name
+                obj = eval("UsersPropifyField").find_by_name(name)
+                obj.destroy!
             end
         
             def propify_all_inner
-                eval("#{self.table_name}_propify_fields").all
+                eval("UsersPropifyField").all
             end
             
             def propify_create_inner name
-                eval("#{self.table_name}_propify_fields").create(:name => name)
+                eval("UsersPropifyField").create(:name => name)
             end
         
-            def create_propify_tables table_name
-               if !ActiveRecord::Base.connection.table_exists? "#{table_name}_propify_fields"
-                    ActiveRecord::Migration.create_table "#{table_name}_propify_fields" do |t|
+            def create_propify_tables
+               if !ActiveRecord::Base.connection.table_exists? "#{self.table_name}_propify_fields"
+                    ActiveRecord::Migration.create_table "#{self.table_name}_propify_fields" do |t|
                       t.string  :name
                     end
                     
-                    ActiveRecord::Migration.create_table " #{table_name}_propify_values" do |t|
+                    ActiveRecord::Migration.create_table " #{self.table_name}_propify_values" do |t|
                       t.string  :value
-                      t.integer "#{table_name}_id"
-                      t.integer "#{table_name}_propify_fields"
+                      t.integer "user_id"
+                      t.integer "users_propify_field_id"
                     end
                 end
             end
             
             def create_propify_classes
                 Object.const_set("UsersPropifyField", Class.new(ActiveRecord::Base))
-                Object.const_set("UsersPropifyValue", Class.new(ActiveRecord::Base))
+                Object.const_set("UsersPropifyValue", Class.new(ActiveRecord::Base) do
+                    has_one "UsersPropifyFields".to_sym
+                    belongs_to "Users".to_sym
+                end)
             end
     end
     
